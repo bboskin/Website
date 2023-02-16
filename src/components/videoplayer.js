123da9bb0d98bdd7bfece22bda9484caa43663fa
Ben Boskin <b.boskin96@gmail.com>
2023-02-16 10:24:21 -0800

    added youtube embeds for PL talks

---
+++ src/components/videoplayer.js
@@ -0,0 +1,21 @@
+import * as React from 'react'
+import YouTube from 'react-youtube'
+
+
+const TalkPlayerA = () => {
+  return (
+    <div>
+    <YouTube videoId='eCI78oYn9Q4' />
+    </div>
+  )
+}
+
+const TalkPlayerB = () => {
+  return (
+    <div>
+    <YouTube videoId='ZqcBLjZLp-Y' />
+    </div>
+  )
+}
+
+export {TalkPlayerA, TalkPlayerB}
--- src/pages/logic.js
+++ src/pages/logic.js
@@ -1,13 +1,22 @@
 import * as React from 'react'
 import Layout from '../components/layout'
-import { StaticImage } from 'gatsby-plugin-image'
+import {TalkPlayerA, TalkPlayerB} from '../components/videoplayer'
+
+
 
 const ResearchPage = () => {
   return (
     <Layout pageTitle="Yeah I was like... really into functional programming for a while">
      <p>TODO talk about Dan </p>
+
      <p> TODO links to Scheme paper, repo, video </p>
 
+     <p> Here's a talk I gave in 2018 at the Scheme and Functional programming
+     Workshop: </p>
+     <TalkPlayerA> </TalkPlayerA>
+     <p> And here's a short talk that I gave while an undergrad at IU talking
+     about work I did with Jeremy Siek. My talk begins at 15:16.</p>
+     <TalkPlayerB> </TalkPlayerB>
      <p> TODO talk about Pete and Northeastern </p>
 
      <p> TODO link to HCOMP paper </p>
