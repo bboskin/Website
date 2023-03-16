f22c58c9b6cc7540159d418e3945c8d5e29e7887
Ben Boskin <b.boskin96@gmail.com>
2023-03-16 10:55:03 -0700

    fixing front page text

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,13 @@ import { picture } from '../components/picture.module.css'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Welcome!">
-    <p> Thanks for coming to my website! I have had a few career paths, and have
-    picked up some hobbies along the way, and I talk about/show them here. I
-    hope you enjoy! I am not the most slick web developer, as is probably evident.
+    <p> Thanks for coming to my website! I have
+    some hobbies, and have had a few career paths, and I talk about/show them here. I
+    hope you enjoy!
     </p>
-    <p>Here's the only headshot I've ever had taken.
-    Looking at it now I kinda have no idea what he's thinking, but we (for now)
-    look pretty similar so hopefully that gives you an idea. </p>
+    <p>Here's a pretty nice picture of me, the only headshot I've ever had taken.
+    It's a few years oldl, so looking at him now I kinda have no idea what he's thinking, but he
+    looks a lot like me so hopefully that gives you an idea. </p>
       <StaticImage className={picture}
         alt="Oops."
         src="../images/headshot.jpeg"
